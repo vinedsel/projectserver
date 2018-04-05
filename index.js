64bd99f7b45dede64034c2772bc33e2293c35061
Raymond Gregory <vinedsel@gmail.com>
2018-04-05 04:54:44 -0500

    Added console statements to all CRUD operations.

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const app = express();
 
 app.post("/", function(req, res) {
   res.send("Data can be created using this method (POST).");
+  console.log("POST was called.");
 });
 
 app.get("/", function(req, res) {
   res.send("GET is used to read information.");
+  console.log("GET was called.");
 });
 
 app.put("/", function(req, res) {
   res.send("Use this (PUT) to update information.");
+  console.log("PUT was called.");
 });
 
 app.delete("/", function(req, res) {
   res.send("Only use DELETE to remove the specified target.");
+  console.log("DELETE was called.");
 });
 
 app.listen(3000, () => {
