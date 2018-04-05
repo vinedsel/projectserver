e1dc166510442093cbb3c5e8c9856757a83c82e5
Raymond Gregory <vinedsel@gmail.com>
2018-04-05 04:38:26 -0500

    Modified index.js file.

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require("express");
 const app = express();
 
 app.post("/", function(req, res) {
-  res.send("You successfully created a POST route!");
+  res.send("Data can be created using this method (POST).");
 });
 
 app.get("/", function(req, res) {
-  res.send("You successfully created a GET route!");
+  res.send("GET is used to read information.");
 });
 
 app.put("/", function(req, res) {
-  res.send("You successfully created a PUT route!");
+  res.send("Use this (PUT) to update information.");
 });
 
 app.delete("/", function(req, res) {
-  res.send("You successfully created a DELETE route!");
+  res.send("Only use DELETE to remove the specified target.");
 });
 
 app.listen(3000, () => {
-  console.log("Simple Server is running on 3000");
+  console.log("Project Server is running on 3000");
 });
